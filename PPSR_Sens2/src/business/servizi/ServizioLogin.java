4e1ef8a8f371e224110dd6ce2883f01a4d9a0a04
frax94 <33449082+frax94@users.noreply.github.com>
2017-11-08 11:42:48 +0100

    aggiornamento

---
+++ PPSR_Sens2/src/business/servizi/ServizioLogin.java
@@ -0,0 +1,47 @@
+package business.servizi;
+
+import business.entita.Utente;
+
+/**
+ * Classe (pattern: singleton) per login e sessione dell'utente
+ * 
+ * @author Andrea
+ *
+ */
+
+public class ServizioLogin {
+	// non ricordo perchè l'ho fatto ma funziona
+	private static final String MENOUNO = "-1";
+	private static Utente utenteLoggato = new Utente(MENOUNO, "", "", "", "", "0", "");
+
+	/**
+	 * Oggetto per l'utente loggato nel sistema
+	 */
+	private ServizioLogin() {
+
+	}
+
+	public static Utente getUtenteLoggato() {
+		return utenteLoggato;
+	}
+
+	public static boolean effettuaLogin(String email, String pwd) {
+		boolean ret = false;
+
+		ServizioUtenti serv = new ServizioUtenti();
+		Utente utenteTemp = serv.controlloIdentita(email, pwd);
+		if (utenteTemp != null) {
+			utenteLoggato = utenteTemp;
+
+			System.out.println(utenteLoggato.getEmail() + " " + utenteLoggato.getPassword() + " "
+					+ utenteLoggato.getAdmin() + " " + utenteLoggato.getCodice());
+
+			ret = true;
+		} else {
+			utenteLoggato = new Utente(MENOUNO, "", "", "", "", "0", "");
+		}
+
+		return ret;
+	}
+
+}
---
+++ PPSR_Sens2/src/presentation/view/ListaImpiantiCodiceController.java
@@ -0,0 +1,113 @@
+package presentation.view;
+
+import java.io.IOException;
+import java.util.List;
+
+import business.servizi.ServizioImpianti;
+import integration.DAO.DaoImpianto.coppia;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+/**
+ * 
+ * @author PPSR
+ *
+ */
+
+public class ListaImpiantiCodiceController {
+
+	private final ServizioImpianti servizio = new ServizioImpianti();
+
+	private ObservableList<coppia> tableImpiantiData = FXCollections.observableArrayList();
+
+	public static coppia impiantoSelezionato;
+
+	public static final String LISTA_SENSORI_CODICE;
+
+	static {
+		impiantoSelezionato = null;
+		LISTA_SENSORI_CODICE = "presentation/view/ListaSensoriCodice.fxml";
+	}
+
+	@FXML
+	private AnchorPane content;
+	@FXML
+	private TableView<coppia> tableImpianti;
+	private AnchorPane newLoadedPane;
+	@FXML
+	private Text textImpianti;
+
+	@SuppressWarnings("unchecked")
+	@FXML
+	public void initialize() {
+		int minLarg = 100;
+		TableColumn<coppia, String> nomeCol = new TableColumn<coppia, String>("Nome Impianto");
+		nomeCol.setMinWidth(minLarg);
+		nomeCol.setCellValueFactory(new PropertyValueFactory<>("nomeI"));
+
+		TableColumn<coppia, String> nomeCCol = new TableColumn<coppia, String>("Nome Cliente");
+		nomeCCol.setMinWidth(minLarg);
+		nomeCCol.setCellValueFactory(new PropertyValueFactory<>("nomeU"));
+
+		TableColumn<coppia, String> cognomeCCol = new TableColumn<coppia, String>("Cognome Cliente");
+		cognomeCCol.setMinWidth(minLarg);
+		cognomeCCol.setCellValueFactory(new PropertyValueFactory<>("cognomeU"));
+
+		tableImpianti.getColumns().addAll(nomeCol, nomeCCol, cognomeCCol);
+
+		if (CodiceClienteController.funzione == 10) {
+			tableImpiantiData = stampaLista(servizio.visualizza(CodiceClienteController.codiceAdmin));
+
+			tableImpianti.setItems(tableImpiantiData);
+
+			tableImpianti.setRowFactory(tv -> {
+				TableRow<coppia> row = new TableRow<>();
+				row.setOnMouseClicked(event -> {
+					if (!row.isEmpty()) {
+						coppia rowData = row.getItem();
+						impiantoSelezionato = rowData;
+						System.out.println(rowData.getI().getNome());
+						goScene(LISTA_SENSORI_CODICE);
+					}
+				});
+
+				return row;
+			});
+		}
+	}
+
+	public void goScene(String scene) {
+		System.out.println("Andrea Past " + scene);
+		try {
+			newLoadedPane = FXMLLoader.load(getClass().getClassLoader().getResource(scene));
+			content.getChildren().clear();
+			content.getChildren().add(newLoadedPane);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Trasforma la lista in una observable list da mostrare nella tabella
+	 * 
+	 * @param lista
+	 *            lista contenente il resultset
+	 * @return observable list da mostrare
+	 * @author andrea
+	 */
+	private ObservableList<coppia> stampaLista(List<coppia> lista) {
+		tableImpiantiData.clear();
+		for (coppia i : lista) {
+			tableImpiantiData.add(i);
+		}
+		return tableImpiantiData;
+	}
+
+}
